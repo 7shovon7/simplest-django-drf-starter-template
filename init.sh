4908cfed8b0941ff44bf4f86a335c9719090d89e
Mahmudur Rahman Shovon <mars@Mahmudurs-Mac-Studio.local>
2024-05-29 10:50:48 +0600

    python and pip command made dynamic

--- init.sh
+++ init.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-# Check if the user provided an argument
+# Default values
+PYTHON="python3"
+PIP="pip3"
+
+# Parse optional arguments
+while getopts ":p:i:" opt; do
+    case $opt in
+        p) PYTHON="$OPTARG"
+        ;;
+        i) PIP="$OPTARG"
+        ;;
+        \?) echo "Invalid option -$OPTARG" >&2
+            exit 1
+        ;;
+    esac
+done
+shift $((OPTIND -1))
+
+# Check if the user provided the required argument
 if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <your preferred name>"
+    echo "Usage: $0 [-p python_executable] [-i pip_executable] <your preferred name>"
     exit 1
 fi
 
@@ -23,7 +41,7 @@ find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
 find . -path "*/migrations/*.pyc"  -delete
 
 # Create virtual environment
-python3 -m venv .venv
+$PYTHON -m venv .venv
 
 # Activate
 source ".venv/bin/activate"
@@ -31,7 +49,7 @@ source ".venv/bin/activate"
 # Install the requirements
 if [ $? -eq 0 ]; then
     echo "Virtual environment activated successfully"
-    pip3 install -r requirements.txt
+    $PIP install -r requirements.txt
 else
     echo "Failed to activate virtual environment"
     exit 1
@@ -41,6 +59,14 @@ fi
 if [ -f ".env.example" ]; then
     cp .env.example .env
     echo ".env file created successfully. Feel free to make changes."
+
+    # Generate a 60-character long secret
+    SECRET_KEY=$($PYTHON -c 'import secrets; print(secrets.token_urlsafe(60))')
+
+    # Update the SECRET_KEY in the .env file
+    sed -i '' "s/^SECRET_KEY=.*/SECRET_KEY=${SECRET_KEY}/" .env
+
+    echo "SECRET_KEY updated successfully"
 else
     echo ".env.example file not found"
     exit 1
